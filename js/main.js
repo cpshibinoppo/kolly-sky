adcf6812e8493a0e28a5a5dc6592fa1605f73603
cpshibinoppo <cpshibinoppo@gmail.com>
2022-06-08 10:45:15 +0530

    9

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function menushow(){
+    var show = document.getElementById('menumaindiv');
+    show.style.visibility = 'visible';
+    show.style.opacity = '1';
+}
+function closemenu(){
+    var show = document.getElementById('menumaindiv');
+    show.style.visibility = 'hidden';
+    show.style.opacity = '0';
+}
