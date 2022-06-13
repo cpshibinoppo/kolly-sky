afa6308ca7cd8196e40ce33b9826004d53f74a2f
Mohammed shibin cp <cpshibinoppo@gmail.com>
2022-06-13 12:00:02 +0530

    slifer

--- js/main.js
+++ js/main.js
@@ -81,34 +81,3 @@ function lovecolor4(){
         one.style.color = 'black';
     }
 }
-
-// function show1(){
-//     btn1.cav.classList.add("mystyle");
-//     var element = document.getElementById('btn2');
-//     element.classList.remove("mystyle");
-//     var remove = document.getElementById('btn3');
-//     remove.classList.remove("mystyle");
-// }
-// function show2(){
-//     btn1.cav.classList.remove("btn1style");
-//     var cav = document.getElementById('btn1');
-//     cav.classList.remove("mystyle");
-//     var cas = document.getElementById('btn1');
-//     cas.classList.add("car1");
-//     var element = document.getElementById('btn2');
-//     element.classList.add("mystyle");
-//     var remove = document.getElementById('btn3');
-//     remove.classList.remove("mystyle");
-// }
-// function show3() {
-//     var cav = document.getElementById('btn1');
-//     cav.classList.remove("btn1style");
-//     var cav = document.getElementById('btn1');
-//     cav.classList.remove("mystyle");
-//     var cas = document.getElementById('btn1');
-//     cas.classList.add("car1");
-//     var remove = document.getElementById('btn2');
-//     remove.classList.remove("mystyle");
-//     var element = document.getElementById("btn3");
-//     element.classList.add("mystyle");
-//   }
